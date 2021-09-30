d3a314c80b872890ecec630ddc4f3ee8c14f40a8
Bhargava36 <70469900+Bhargava36@users.noreply.github.com>
2021-09-30 16:01:30 +0530

    2nd commit

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 
@@ -13,7 +14,7 @@
 	<!-- Favicon  -->
 	<link rel="icon" href="./assets/images/sac-favicon.png">
 	<!-- Bootstrap CSS CDN -->
-	<link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/css/bootstrap.min.cssn">
 	<!-- Font awesome -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 	<!-- Bootstrap CSS CDN -->
@@ -85,8 +86,10 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 		<!-- Bootstrap JS -->
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-
+        
+        <!-- backscript link -->
 		<script src="./script.js"></script>
+        <!-- linkk -->
 		<script>
 			$(document).ready(function () {
 				$('#sidebarCollapse').on('click', function () {
@@ -94,6 +97,7 @@
 				});
 			});
 		</script>
-		<body>
+        
+	<body>
 
 </html>
