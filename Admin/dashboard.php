1ab3353a9cf75e143536afe485ad2d0ac029789e
ASHOK VARMA BEVARA <70469886+Varma1026@users.noreply.github.com>
2021-09-30 17:48:35 +0530

    trash

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 
@@ -14,11 +13,7 @@
 	<!-- Favicon  -->
 	<link rel="icon" href="./assets/images/sac-favicon.png">
 	<!-- Bootstrap CSS CDN -->
-<<<<<<< HEAD
-	<link rel="stylesheet" href="/assets/css/bootstrap.min.cssn">
-=======
 	<link rel="stylesheet" href="./assets/css/bootstrap.min.css">
->>>>>>> parent of 34c1c26 (login and admin loginadded)
 	<!-- Font awesome -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 	<!-- Bootstrap CSS CDN -->
@@ -90,10 +85,8 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 		<!-- Bootstrap JS -->
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-        
-        <!-- backscript link -->
+
 		<script src="./script.js"></script>
-        <!-- linkk -->
 		<script>
 			$(document).ready(function () {
 				$('#sidebarCollapse').on('click', function () {
@@ -101,7 +94,6 @@
 				});
 			});
 		</script>
-        
-	<body>
+		<body>
 
 </html>
